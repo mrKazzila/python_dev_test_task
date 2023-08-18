e0c6b211abe9266783872fbb18c15a56fad68d11
mrKazzila <mrkazzila@gmail.com>
2023-08-19 00:53:19 +0300

    feat: add & update services functions

--- app/code_files/code_files_services.py
+++ app/code_files/code_files_services.py
@@ -2,6 +2,17 @@ from datetime import datetime
 
 from django.core.paginator import Paginator
 
+from code_files.models import CodeCheck, CodeCheckStatus, FileState
+
+
+def create_files_with_check_status(files):
+    files_with_checks = []
+    for file in files:
+        code_check = CodeCheck.objects.filter(file=file).first()
+        files_with_checks.append((file, code_check))
+
+    return files_with_checks
+
 
 def create_paginator_obj(files, page_number):
     """
@@ -46,17 +57,19 @@ def delete_file(model, file_pk, user):
     uploaded_file = model.objects.get(pk=file_pk)
 
     if uploaded_file.user == user:
+        uploaded_file.file.delete(save=False)
         uploaded_file.delete()
 
 
-def file_manager(form, new_file_name, user, old_file):
+def file_manager(form, user, new_file_name, old_file):
     """
     Manage file operations based on the presence of an old file.
 
     Args:
         form: The form object containing file data.
-        new_file_name (str): Name of the new file.
         user: The user associated with the file.
+        new_file_name (str): Name of the new file.
+
         old_file: The old file object to update, if exists.
     """
     is_old_file_name_exist = int(bool(old_file))
@@ -66,27 +79,29 @@ def file_manager(form, new_file_name, user, old_file):
         1: update_exist_file,
     }
 
-    file_operations.get(is_old_file_name_exist)(form, new_file_name, user, old_file)
+    file_obj = file_operations.get(is_old_file_name_exist)(form, user, new_file_name, old_file)
+    CodeCheck.objects.create(file=file_obj, status=CodeCheckStatus.UNCHECKED.value)
 
 
-def add_new_file(form, new_file_name, user, old_file=None):
+def add_new_file(form, user, new_file_name, old_file=None):
     """
     Add a new file entry to the database.
 
     Args:
         form: The form object containing file data.
-        new_file_name (str): Name of the new file.
         user: The user associated with the file.
+        new_file_name (str): Name of the new file.
         old_file: The old file object, if available.
     """
     new_file = _create_file_odj(
         form_odj=form,
         user=user,
-        is_new_status=True,
         filename=new_file_name,
-        is_checked_status=False,
+        state=FileState.NEW.value,
     )
+
     new_file.save()
+    return new_file
 
 
 def update_exist_file(form, new_file_name, user, old_file):
@@ -95,23 +110,25 @@ def update_exist_file(form, new_file_name, user, old_file):
 
     Args:
         form: The form object containing file data.
-        new_file_name (str): Name of the new file.
         user: The user associated with the file.
+        new_file_name (str): Name of the new file.
         old_file: The old file object to update.
     """
     new_uploaded_file = _create_file_odj(
         form_odj=form,
         user=user,
-        is_new_status=False,
         filename=new_file_name,
-        is_checked_status=False,
+        state=FileState.OVERWRITTEN.value,
     )
     new_uploaded_file.uploaded_at = datetime.now()
+    old_file.file.delete(save=False)
     old_file.delete()
+
     new_uploaded_file.save()
+    return new_uploaded_file
 
 
-def _create_file_odj(form_odj, user, filename, is_new_status, is_checked_status):
+def _create_file_odj(form_odj, user, filename, state):
     """
     Create a file object from the given form data.
 
@@ -119,16 +136,13 @@ def _create_file_odj(form_odj, user, filename, is_new_status, is_checked_status)
         form_obj: The form object containing file data.
         user: The user associated with the file.
         filename (str): Name of the file.
-        is_new_status (bool): Indicates if the file is new.
-        is_checked_status (bool): Indicates if the file is checked.
-
+        state: File state
     Returns:
         File object: The created file object.
     """
     file_obj = form_odj.save(commit=False)
     file_obj.user = user
-    file_obj.is_new = is_new_status
     file_obj.filename = filename
-    file_obj.is_checked = is_checked_status
+    file_obj.state = state
 
     return file_obj
