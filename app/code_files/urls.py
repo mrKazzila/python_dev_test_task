fa04b964c3ce863fd315835a47c50685663f1da3
mrKazzila <mrkazzila@gmail.com>
2023-08-18 15:39:27 +0300

    feat: update url for delete

--- app/code_files/urls.py
+++ app/code_files/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import FileManagementView, delete_file
+from .views import DeleteFileView, FileManagementView
 
 app_name = 'code_files'
 
 urlpatterns = [
     path('list/', FileManagementView.as_view(), name='file_list'),
-    path('delete/<int:file_id>/', delete_file, name='delete_file'),
+    path('delete/<int:file_id>/', DeleteFileView.as_view(), name='delete_file'),
 ]
