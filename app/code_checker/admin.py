443e78f563dc8d86b2a741c1042e842648fee0d2
mrKazzila <mrkazzila@gmail.com>
2023-08-21 17:39:38 +0300

    feat: register models for admin

--- app/code_checker/admin.py
+++ app/code_checker/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from code_checker.models import CheckLog, CodeCheck
+
+
+@admin.register(CodeCheck)
+class CodeCheckAdmin(admin.ModelAdmin):
+    """Register the CodeCheck model and settings fields for admin."""
+
+    list_display = ('file',)
+    fields = (
+        ('file', 'timestamp'),
+        ('status', 'is_notified'),
+        'last_check_result',
+    )
+
+
+@admin.register(CheckLog)
+class CheckLogAdmin(admin.ModelAdmin):
+    """Register the CheckLog model and settings fields for admin."""
+
+    list_display = ('code_check',)
+    fields = ('code_check',)
